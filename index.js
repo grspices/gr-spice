3620a463ffa415b766daa37cd2f8171283bab4ab
Rabdeep singh kharbanda <rabdeep.s@mobstac.com>
2023-10-08 21:59:34 +0530

    Initial commit  for gr spice pricing

---
+++ index.js
@@ -0,0 +1,83 @@
+// atroposJS parameters
+// rotateLock: true,
+// rotateXMax: 15,
+// rotateYMax: 15,
+// shadow: true,
+// shadowOffset: 10,
+// shadowScale: 0.8,
+// highlight: false,
+// durationEnter: 600,
+// activeOffset: 60
+
+const sonth = Atropos({
+    el: '.sonth',
+    // rest of parameters
+    rotateLock: true,
+
+    // rotateXMax: 75,
+    // rotateYMax: 75,
+
+    shadow: true,
+    shadowOffset: 50,
+    // shadowScale: 5,
+    // highlight: false,
+    // durationEnter: 1000,
+    // activeOffset: 560
+});
+
+
+const dalchini = Atropos({
+    el: '.dalchini',
+    rotateLock: true,
+    shadow: true,
+    shadowOffset: 50,
+});
+
+const shahjeera = Atropos({
+    el: '.shahjeera',
+    rotateLock: true,
+    shadow: true,
+    shadowOffset: 50
+});
+
+const badiyanPhool = Atropos({
+    el: '.badiyanPhool',
+    rotateLock: true,
+    shadow: true,
+    shadowOffset: 50
+});
+
+const safedMirchi = Atropos({
+    el: '.safedMirchi',
+    rotateLock: true,
+    shadow: true,
+    shadowOffset: 50
+});
+
+const laung = Atropos({
+    el: '.laung',
+    rotateLock: true,
+    shadow: true,
+    shadowOffset: 50
+});
+
+const jaiphal = Atropos({
+    el: '.jaiphal',
+    rotateLock: true,
+    shadow: true,
+    shadowOffset: 50
+});
+
+const pattharPhool = Atropos({
+    el: '.pattharPhool',
+    rotateLock: true,
+    shadow: true,
+    shadowOffset: 50
+});
+
+const badiElaichi = Atropos({
+    el: '.badiElaichi',
+    rotateLock: true,
+    shadow: true,
+    shadowOffset: 50,
+});
